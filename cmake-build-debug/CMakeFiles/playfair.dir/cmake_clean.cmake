eb699188005b746e1b04af3ed0b0dc44de9d9dba
Tommaso Catervi <56674392+TommasoCatervi@users.noreply.github.com>
2020-11-13 19:38:05 +0100

    Creazione progetto

---
+++ cmake-build-debug/CMakeFiles/playfair.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/playfair.dir/main.c.obj"
+  "libplayfair.dll.a"
+  "playfair.exe"
+  "playfair.exe.manifest"
+  "playfair.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/playfair.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
