7fa54877c0524c4a2d3c6633bfb055f80ffaf058
PablitoParolito <74489190+PablitoParolito@users.noreply.github.com>
2020-11-15 10:50:05 +0100

    Prova
    Prova2

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Tommy/Documents/CLion Workspace/progettoLSO
+# Install script for directory: C:/Users/Paolo/CLionProjects/progettoLSO
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Tommy/Documents/CLion Workspace/progettoLSO/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Paolo/CLionProjects/progettoLSO/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
